2af04ccfec4775b82cf340d2dcdefbc9a219fd4f
DassiAhmed <75790279+DassiAhmed@users.noreply.github.com>
2021-01-04 23:51:44 -0800

    Add files via upload

---
+++ src/vols.h
@@ -0,0 +1,18 @@
+
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+enum
+{ DEPART,
+DESTINATION,
+DATEDEPART,
+DATERETOUR,
+NUM_COLL
+};
+
+
+
+void affichage_vols (GtkWidget *treeview3) ;
+void recherchervols(char depart[300]);
